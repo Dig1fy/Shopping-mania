007ea9036f0d4d2c3f5b49ef4da2dcf75c94aa5d
Ivaylo Petrov <ivaylozp@gmail.com>
2021-09-24 13:14:31 +0300

    Initial commit

---
+++ src/components/item/Item.styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+
+export const Wrapper = styled.div`
+display:flex;
+justify-content: space-between;
+flex-direction: column;
+width: 100%;
+border:1px solid lightblue;
+border-radius:20px;
+height: 100%;
+
+button{
+    border-radius: 0 0 10px 10px;
+}
+
+img{
+    max-height: 250px;
+    max-width: 250px;
+}
+`;
